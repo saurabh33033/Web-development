8a32a56431a90325c6667988cb68a90112a72993
saurabh <saurabh@trapkiller.com>
2021-03-30 12:27:02 +0530

    added tut53.html and tut53.js

---
+++ tut53.js
@@ -0,0 +1,23 @@
+console.log("This is tutorials 53");
+let name = "Saurabh";
+let name1 = "Shubham";
+let name2 = "Rohan";
+let name3 = "Sagar";
+
+function greet(name, greetText){
+    console.log(name + " is a good boy.")
+    console.log(greetText + " " + name)
+}
+let greetText = "Good Morning"
+
+function sum(a, b, c){
+    let d = a + b + c;
+    return d;
+}
+
+// greet(name, greetText)
+// greet(name1, greetText)
+// greet(name2, greetText)
+// greet(name3, greetText)
+let ret = sum(1,2,3)
+console.log(ret)
