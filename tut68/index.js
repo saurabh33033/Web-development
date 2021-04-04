11067da1e9f4b02d494225f1af5097629377afb5
saurabh <saurabh@trapkiller.com>
2021-04-04 19:52:54 +0530

    Added tut68

---
+++ tut68/index.js
@@ -0,0 +1,6 @@
+// const average = require("./mod")
+// console.log(average([3,8]))
+const mod = require("./mod")
+console.log(mod.name)
+// console.log(mod.avg([3,8]))
+console.log("This is index.js");
